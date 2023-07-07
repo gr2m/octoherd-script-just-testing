72b05110d797fd600fe394f2fb0f353162ab389f
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2023-07-07 15:27:47 -0700

    feat: initial version

---
+++ script.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/**
+ * just testing
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ */
+export async function script(octokit, repository) {}
